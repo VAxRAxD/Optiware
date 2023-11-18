84a47973c02bb4513809dc959d0bd95dc5b66a8b
Varad Prabhu <varadp763@gmail.com>
2023-11-18 19:47:36 +0530

    Manufacturing API testing

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import dashboards
+from . import dashboards,orders
 
 urlpatterns = [
     path('dashboard/',dashboards.dashboard,name="dashboard"),
-    path('',dashboards.store,name='store'),
+    path('store/',dashboards.store,name='store'),
+    path('manufacture/',orders.startManufacturing,name='manufacturing')
 ]
