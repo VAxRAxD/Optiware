63413c7f9bb13dc7d07896386a711d13377cc6a4
Varad Prabhu <varadp763@gmail.com>
2023-11-26 11:45:23 +0530

    Migrated to RDS

--- Optiware/settings.py
+++ Optiware/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'Optiware.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'optiware',
+        'USER': 'admin',
+        'PASSWORD': 'Password1234',
+        'HOST': 'endpoint',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
