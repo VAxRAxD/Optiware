8dd3a27eca8fc784e0329342f9614aaaf1b1a802
LightningRex <lanceferns22@gmail.com>
2024-04-21 13:32:01 +0530

    invoice pdf download

--- App/invoice.py
+++ App/invoice.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse, HttpResponse
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Spacer
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from . models import *
 import datetime
 
@@ -25,17 +26,58 @@ def downloadInvoicePdf(request, id):
     response['Content-Disposition'] = f'attachment; filename="invoice.pdf'
     custom_page_width = 5 * inch
     custom_page_height = 4.5 * inch
-    doc = SimpleDocTemplate(response, pagesize=(custom_page_width, custom_page_height))
+    doc = SimpleDocTemplate(response, pagesize=(custom_page_width, custom_page_height),
+                            leftMargin=30,
+                            rightMargin=20,
+                            topMargin=20,
+                            bottomMargin=20)
+    
+    title_style = ParagraphStyle(
+            name='TitleStyle',
+            fontSize=22,
+            textColor=colors.darkgoldenrod,
+            leading=30,  # line spacing
+            fontName="Helvetica-Bold",
+            background=colors.lightcoral
+    )
+
     styles = getSampleStyleSheet ()
     elements = []
-    elements.append(Paragraph("Invoice", styles['Heading1']))
-    elements.append(Paragraph("Company Name: Divine Enterprises", styles['Heading2']))
+    elements.append(Paragraph("Divine Enterprises Invoice", title_style))
+    elements.append(Spacer(1, 2))
+    elements.append(Paragraph(f"Order ID: {id}", styles["Heading2"]))
+    elements.append(Spacer(1, 10))
     elements.append(Paragraph(f"Customer Name: {invoice_data['Customer_name']}", styles["Normal"]))
+    elements.append(Spacer(1, 2))
     elements.append(Paragraph(f"Invoice Amount: {invoice_data['Invoice_amount']}", styles["Normal"]))
+    elements.append(Spacer(1, 2))
     elements.append(Paragraph(f"Invoice Date: {invoice_data['Invoice_date']}", styles["Normal"]))
+    elements.append(Spacer(1, 2))
     elements.append(Paragraph(f"Product Name: {invoice_data['Product_name']}", styles["Normal"]))
+    elements.append(Spacer(1, 2))
     elements.append(Paragraph(f"Order Quantity: {invoice_data['Order_quantity']}", styles["Normal"]))
-    elements.append(Paragraph(f"Order Date: {invoice_data['Order_date']}", styles["Normal"]))
-    elements.append(Paragraph(f"Delivery Date: {invoice_data['Delivery_date']}", styles["Normal"]))
+    elements.append(Spacer(1, 2))
+    #     ["Customer Name", invoice_data['Customer_name']],
+    #     ["Invoice Amount", invoice_data['Invoice_amount']],
+    #     ["Invoice Date", invoice_data['Invoice_date']],
+    #     ["Product Name", invoice_data['Product_name']],
+    #     ["Order Quantity", invoice_data['Order_quantity']],
+    #     ["Order Date", invoice_data['Order_date']],
+    #     ["Delivery Date", invoice_data['Delivery_date']],
+    # ]
+
+    # # Create table
+    # table = Table(data)
+    # table.setStyle(TableStyle([
+    #     ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+    #     ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    #     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    #     ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    #     ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    #     ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+    #     ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    # ]))
+    # elements.append(table)
+
     doc.build(elements)
     return response
