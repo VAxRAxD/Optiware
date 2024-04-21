f42a50788f4220a73442bf5c1ec8b4430f0fbef0
Varad Prabhu <varadp763@gmail.com>
2024-04-21 11:49:38 +0530

    Fixed Page size issue

--- App/invoice.py
+++ App/invoice.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse, HttpResponse
-from reportlab.lib.pagesizes import letter, A4
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from . models import *
 import datetime
-import json
 
 def getInvoiceData(id):
     order_obj = Order.objects.get(id=id)
@@ -16,33 +16,26 @@ def getInvoiceData(id):
         "Order_date": order_obj.ordered_date,
         "Delivery_date": order_obj.delivered_date,
         "Invoice_date": datetime.datetime.now()
-        }
-    # print(data)
-    # return HttpResponse(json.dumps(data, default=str))
+    }
     return data
 
 def downloadInvoicePdf(request, id):
     invoice_data = getInvoiceData(id)
-    print(type(invoice_data))
-    print(invoice_data)
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="invoice_{invoice_data['Customer_name']}.pdf"'
-    
-    doc = SimpleDocTemplate(response, pagesize=letter)
+    response['Content-Disposition'] = f'attachment; filename="invoice.pdf'
+    custom_page_width = 5 * inch
+    custom_page_height = 4.5 * inch
+    doc = SimpleDocTemplate(response, pagesize=(custom_page_width, custom_page_height))
     styles = getSampleStyleSheet ()
-    elements = [Paragraph("Invoice", styles['Heading1']),
-                Paragraph(f"Company Name: Divine Enterprises", styles['Heading2']),
-                Paragraph(f"Customer_name: {invoice_data['Customer_name']}", styles["Normal"]),
-                Paragraph(f"Invoice_amount: {invoice_data['Invoice_amount']}", styles["Normal"]),
-                Paragraph(f"Invoice_date: {invoice_data['Invoice_date']}", styles["Normal"]),
-                Paragraph(f"Product_name: {invoice_data['Product_name']}", styles["Normal"]),
-                Paragraph(f"Order_quantity: {invoice_data['Order_quantity']}", styles["Normal"]),
-                Paragraph(f"Order_date: {invoice_data['Order_date']}", styles["Normal"]),
-                Paragraph(f"Delivery_date: {invoice_data['Delivery_date']}", styles["Normal"])]
-
-    # Add other fields from invoice_data to the PDF
-    print("This is elements var ", elements)
-    # Build the PDF document
+    elements = []
+    elements.append(Paragraph("Invoice", styles['Heading1']))
+    elements.append(Paragraph("Company Name: Divine Enterprises", styles['Heading2']))
+    elements.append(Paragraph(f"Customer Name: {invoice_data['Customer_name']}", styles["Normal"]))
+    elements.append(Paragraph(f"Invoice Amount: {invoice_data['Invoice_amount']}", styles["Normal"]))
+    elements.append(Paragraph(f"Invoice Date: {invoice_data['Invoice_date']}", styles["Normal"]))
+    elements.append(Paragraph(f"Product Name: {invoice_data['Product_name']}", styles["Normal"]))
+    elements.append(Paragraph(f"Order Quantity: {invoice_data['Order_quantity']}", styles["Normal"]))
+    elements.append(Paragraph(f"Order Date: {invoice_data['Order_date']}", styles["Normal"]))
+    elements.append(Paragraph(f"Delivery Date: {invoice_data['Delivery_date']}", styles["Normal"]))
     doc.build(elements)
-    print(elements)
     return response
